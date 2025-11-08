d54d33e315f4c66dab6a04c607ac94e969666894
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-11-09 02:56:00 +0600

    added volume four

---
+++ M02V04.js
@@ -0,0 +1,13 @@
+// Some
+
+const numbers = [1, 2, 3, 4, 5]
+
+const hasEvenNumber = numbers.some(number => number % 2 === 0)
+
+console.log(hasEvenNumber);
+
+
+const currentUserRoles = ["user", "editor"]
+const featureAccessRoles = ["admin", "manager"]
+
+// Array.from()
